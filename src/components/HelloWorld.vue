2b2be519bb201c9b36b7d69ec02bd6aa471df8b5
sooomin17 <hongpodo1717@gmail.com>
2021-07-23 18:00:11 +0900

    vue3 webpack tamplate

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2 @click="$emit('click')">
+    {{ msg }}
+  </h2> 
+  <img
+    src="~assets/img.jpg"
+    alt="겨울" />
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: ''
+    }
+  },
+  emits: ['click']
+}
+</script>
+
+<style scoped lang="scss">
+  h2 {
+    font-size: 22px;
+    color: purple;
+    & + img {
+      width: 120px;
+    }
+  }
+</style>
